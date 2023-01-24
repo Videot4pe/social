ff1d2e9efc1a2daae6e3d65b489406dbbcfd9b89
Alexandr Kvasnikov <videot4pe@gmail.com>
2023-01-24 13:04:02 +0300

    dockerfile

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page>
-    <div :style="!isMobile ? 'display: grid; grid-template-columns: 580px 280px; grid-gap: 20px' : 'padding-bottom: 150px'">
+    <div class="index-page" :style="!isMobile ? 'display: grid; grid-template-columns: 580px 280px; grid-gap: 20px' : 'padding-bottom: 150px'">
       <div>
         <company-card class="full-width" />
         <feed-list class="q-mt-lg" />
@@ -23,3 +23,7 @@ const isMobile = computed(() => $q.platform.is.mobile)
 
 
 </script>
+
+<style scoped lang="scss">
+
+</style>
